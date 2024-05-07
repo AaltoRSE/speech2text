7ab4c75402528c9d4a8d9cfb29ba910c553eb7f2
hsnfirooz <hsnfirooz@gmail.com>
2024-05-07 10:35:01 +0300

    add v3 submission form for OOD

--- bin/deploy-data/ood/template/script.sh
+++ bin/deploy-data/ood/template/script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-export PATH="/share/apps/manual_installations/speech2text/<VERSION>/env/bin:/usr/lib64/ccache:/usr/local/bin:/usr/bin:/home/firoozh1/bin:/usr/local/sbin:/usr/sbin:/opt/ibutils/bin:/opt/dell/srvadmin/bin"
+export PATH="/appl/manual_installations/software/speech2text/<VERSION>/env/bin"
 
 export HF_HOME="<HF_HOME>"
 export PYANNOTE_CACHE="<PYANNOTE_CACHE>"
@@ -15,4 +15,4 @@ export SPEECH2TEXT_TMP="<SPEECH2TEXT_TMP>"
 
 export HF_HUB_OFFLINE="1"
 
-speech2text $folder_path
+speech2text $audio_path
