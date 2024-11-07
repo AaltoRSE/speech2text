5cff2ad2c10e34922a8eb9541bb20715b7bf59c9
hsnfirooz <hsnfirooz@gmail.com>
2024-11-07 09:05:05 +0200

    add dynamic ood form values
    Instead of hardcoding the available languages and whisper models, values are fetched from the speech2text setting file and added to the ood form

---
+++ bin/deploy-data/update_ood_form_values.py
@@ -0,0 +1,29 @@
+import yaml
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+from src.settings import (supported_languages, 
+                          available_whisper_models, 
+                          default_whisper_model)
+
+# Path to the form.yml file
+form_yml_path = 'bin/deploy-data/ood/form1.yml'
+
+# Load the existing form.yml
+with open(form_yml_path, 'r') as file:
+    form_data = yaml.safe_load(file)
+
+# Update the language_field options with supported languages
+form_data['attributes']['language_field']['options'] = list(supported_languages.keys())
+
+# Update the model_selector options with available whisper models
+form_data['attributes']['model_selector']['options'] = [
+    ["default", default_whisper_model]
+] + [[model, model] for model in available_whisper_models]
+
+# Write the updated data back to form.yml
+with open(form_yml_path, 'w') as file:
+    yaml.dump(form_data, file, default_flow_style=False, sort_keys=False)
+
+print("form.yml has been updated with supported languages from settings.py.")
